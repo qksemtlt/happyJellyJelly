78be90bc893ecd027e67b2a40efb4cd774acf1ac
Jeremy-Fe <snrn221@naver.com>
2024-07-21 22:45:02 +0900

    commit - 브랜치 접근 신우철 (새롱운 파일도 만들었슴ㅇㅇ므)

--- src/main/java/com/ex/controller/TestConteroller.java
+++ src/main/java/com/ex/controller/TestConteroller.java
@@ -18,5 +18,8 @@ public class TestConteroller {
 			System.out.println("브랜치 새로 만들었습니다. 기모링");
 			
 			System.out.println("마스터에 접근한다 으하하ㅏ하하하하하하ㅏ~@!");
+			System.out.println("동현이 코드도 가져왔다 !!!!!!!!!!!!!!!!!!!!!!!!");
+			
+			System.out.println("브랜치 접근 으하하하~!");
 		}
 }
---
+++ src/main/java/com/ex/controller/test.java
@@ -0,0 +1,7 @@
+package com.ex.controller;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("이것은 신우청빈디ㅏ");
+	}
+}

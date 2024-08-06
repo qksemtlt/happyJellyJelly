55b31ea5d6efa5c72815878c5127efb9d6508911
LEEJIHYEON <LEEJIHYEON@192.168.219.206>
2024-08-06 14:12:40 +0900

    ticket 페이징 처리 및 ticketcount추가 등 수정 memberEntity 미연 수정 반영 navbar.html 이용권 링크 반영

--- src/main/java/com/ex/service/TicketService.java
+++ src/main/java/com/ex/service/TicketService.java
@@ -2,9 +2,12 @@ package com.ex.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.ex.data.TicketDTO;
@@ -20,9 +23,15 @@ public class TicketService {
 	@Autowired
 	private final TicketRepository ticketRepository;
 	
+	public Page<TicketEntity> getTicketsAll(int page){
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add((Sort.Order.desc("id").desc("salesstatus")));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		return ticketRepository.findAll(pageable);
+	}
+	
 	// 판매중인 이용권 티켓만 목록 조회 (판매상태=1)
 	public List<TicketDTO> getTicketsList(){
-		System.out.println("TicketService 진입 getTicketsList()");
 		List<TicketDTO> list = null;
 		TicketDTO tDto = null;
 		List<TicketEntity> dbTicketList = ticketRepository.findBySalesstatus(1);
@@ -35,6 +44,7 @@ public class TicketService {
 					.groupType(t.getGroupType())
 					.salesstatus(t.getSalesstatus())
 					.dayofweek(t.getDayofweek())
+					.ticketcount(t.getTicketcount())
 					.build();
 			
 			list.add(tDto);
@@ -52,6 +62,7 @@ public class TicketService {
 							.groupType(te.getGroupType())
 							.salesstatus(te.getSalesstatus())
 							.dayofweek(te.getDayofweek())
+							.ticketcount(te.getTicketcount())
 							.build();
 		return ticketDto;
 		
@@ -59,12 +70,11 @@ public class TicketService {
 	
 	// 이용권 추가
 	public void addTicket(TicketDTO ticketDTO) {
-		
 		String day;
 		if (ticketDTO.getGroupType().equals("DAYCARE")) {
 		    day = null;
 		} else {
-		    day = ticketDTO.getDayofweek();
+			day = ticketDTO.getDayofweek().replace(",","");
 		}
         TicketEntity ticket = TicketEntity.builder()
         						.ticketname(ticketDTO.getTicketname())
@@ -72,9 +82,22 @@ public class TicketService {
 						        .groupType(ticketDTO.getGroupType())
 						        .salesstatus(1)
 						        .dayofweek(day)
+						        .ticketcount(ticketDTO.getTicketcount())
 						        .build();
         
         ticketRepository.save(ticket);
     }
+	
+	// 이용권 수정 (판매상태만 변경)
+	public void updateTicket(TicketDTO ticketDTO, Integer id) {
+		TicketEntity te = ticketRepository.findById(id).orElseThrow(() -> new RuntimeException("Ticket not found"));
+//		te.setTicketname(ticketDTO.getTicketname());
+//		te.setPrice(ticketDTO.getPrice());
+//		te.setGroupType(ticketDTO.getGroupType());
+		te.setSalesstatus(ticketDTO.getSalesstatus());
+//		te.setDayofweek(ticketDTO.getDayofweek());
+		
+		ticketRepository.save(te);
+	}
 
 }

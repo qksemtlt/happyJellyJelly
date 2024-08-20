e75425dc44f5da79bdd5bfaff5d283e34e0b6a28
솜팡이바보 <@192.168.219.142>
2024-08-20 17:57:03 +0900

    동현 원본

--- happyJelly/src/main/java/com/ex/repository/MonthcareGroupsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ex.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ex.entity.MonthcareGroupsEntity;
-
-@Repository
-public interface MonthcareGroupsRepository extends JpaRepository<MonthcareGroupsEntity, Integer>{
-
-}

4828f9938c1012605253f19b8da24501b05bb94c
qksemtlt <qksemtlt@gmail.com>
2024-08-21 15:30:27 +0900

    Delete happyJelly directory

--- happyJelly/src/main/java/com/ex/repository/VaccinationsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ex.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ex.entity.VaccinationsEntity;
-
-@Repository
-public interface VaccinationsRepository  extends JpaRepository<VaccinationsEntity, Integer>{
-
-}
